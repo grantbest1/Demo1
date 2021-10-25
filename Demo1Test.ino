974a2c6c6e63d005eed11a5df2357a9140976760
Curry Gardner <58348205+CurlyFry53@users.noreply.github.com>
2021-10-25 15:12:26 -0600

    With phi angle

--- Demo1Test.ino
+++ Demo1Test.ino
@@ -17,6 +17,7 @@ DualMC33926MotorShield md;
 int counter_left = 0;
 int oldCount_left = 0;
 int newCount_left = 0;
+int deltaCount_left = 0;
 float rad_left;
 int currentState_left;
 int lastState_left;
@@ -26,10 +27,21 @@ int tOld_left;
 int deltaT_left;
 
 
+float v_LEFT = 0; // velocity of left encoder
+float v_RIGHT = 0; // velocity of right encoder
+float deltaDistance_LEFT = 0; // change in left distance
+float deltaDistance_RIGHT = 0; // change in right distance
 
+float x_DISTANCE = .001; //total x distance 
+float y_DISTANCE = .001; // total y distance 
+float phi = .001; // current angle from straight
+
+
+//right variables
 int counter_right = 0;
 int oldCount_right = 0;
 int newCount_right = 0;
+int deltaCount_right = 0;
 float rad_right;
 int currentState_right;
 int lastState_right;
@@ -38,7 +50,8 @@ int tOld_right;
 int tNew_right;
 int deltaT_right;
 
-
+float r = 0.25;
+float b = 0.91;
 
 //// angular velocity
 double thetaDot_left; 
@@ -61,13 +74,34 @@ void stopIfFault()
 //setup function
 void setup() {
   //baud rate set to 250,000
-  Serial.begin(250000);
+  Serial.begin(9600);
   pinMode(clk_left, INPUT);
   pinMode(clk_right, INPUT);
   pinMode(dt_left, INPUT);
   pinMode(dt_right, INPUT);
 
   Serial.println("Dual MC33926 Motor Shield");
+  
+//    Serial.print("LEFT: ");
+//    Serial.print("counter_left");
+//    Serial.print(" ");
+//    Serial.print("rad_left");
+//    Serial.print(" ");
+//    Serial.print("thetaDot_left");
+//    Serial.print(" ");
+//    Serial.print("x_DISTANCE");
+//    Serial.print("    RIGHT: ");
+//    Serial.print("counter_right");
+//    Serial.print(" ");
+//    Serial.print("rad_right");
+//    Serial.print(" ");
+//    Serial.print("thetaDot_right");
+//    Serial.print(" ");
+//    Serial.print("y_DISTANCE");
+//    Serial.print("      ");
+//    Serial.print("phi");
+//  Serial.println();
+  
   md.init();
   tOld_left = millis();
   tOld_right = millis();
@@ -87,23 +121,42 @@ void setup() {
 void loop() {
   if(flag == true){
       Serial.print("LEFT: ");
-      Serial.print(counter_left);
-      Serial.print(" ");
-      Serial.print(rad_left);
-      Serial.print(" ");
+//      Serial.print(counter_left);
+//      Serial.print(" ");
+//      Serial.print(rad_left);
+//      Serial.print(" ");
       Serial.print(thetaDot_left);
+      Serial.print(" ");
+      Serial.print(v_LEFT);
+      Serial.print(" ");
+      Serial.print(x_DISTANCE);
+      Serial.print("    RIGHT: ");
+//      Serial.print(counter_right);
+//      Serial.print(" ");
+//      Serial.print(rad_right);
+//      Serial.print(" ");
+      Serial.print(thetaDot_right);
+      Serial.print(" ");
+      Serial.print(v_RIGHT);
+      Serial.print(" ");
+      Serial.print(y_DISTANCE);
+      Serial.print("      ");
+      Serial.print(phi);
+//      Serial.print(deltaT_left);
+//      Serial.print(" ");
+//      Serial.print(deltaCount_left);
 //      Serial.print(" ");
 //      Serial.print(thetaDot_left);
-      Serial.print("    RIGHT: ");
-      Serial.print(counter_right);
-      Serial.print(" ");
-      Serial.print(rad_right);
-      Serial.print(" ");
-      Serial.print(thetaDot_right);
+//      Serial.print(" ");
+//      Serial.print(deltaT_right);
+//      Serial.print(" ");
+//      Serial.print(deltaCount_right);
+//      Serial.print(" ");
+//      Serial.print(thetaDot_right);
       Serial.println();
       flag = false;
   }
-  if(counter_left >=2400 && counter_right >=2400){
+  if(counter_left >=800 && counter_right >=800){
     md.setM1Speed(0);
     md.setM2Speed(0);
   }else{
@@ -125,8 +178,15 @@ void updateLeftEncoder(){
   if(currentState_left != lastState_left && currentState_left == 1){
     tNew_left = millis();
     deltaT_left = tNew_left - tOld_left;
+    deltaCount_left = newCount_left-oldCount_left;
     //thetadot is the angular velocity
-    thetaDot_left = ((newCount_left-oldCount_left)*1000*2*3.1415)/((deltaT_left)*800);
+    thetaDot_left = abs((deltaCount_left*1000*2*3.1415)/((deltaT_left)*800));
+
+    v_LEFT = thetaDot_left * r; // actual velocity of the left wheel is calculated
+    deltaDistance_LEFT = v_LEFT * deltaT_left/1000; // the distance the left wheel travels per clicks
+    x_DISTANCE += (cos(phi)*(deltaDistance_LEFT + deltaDistance_RIGHT))/2; // the full x distance the wheel travels
+    y_DISTANCE += (sin(phi)*(deltaDistance_LEFT + deltaDistance_RIGHT))/2; // full y distance the wheel
+    phi += (r/b)*(deltaDistance_LEFT - deltaDistance_RIGHT); // calculates the new angle from perfectly straight
     
     oldCount_left = counter_left;
     if(digitalRead(dt_left) != currentState_left){
@@ -155,8 +215,16 @@ void updateRightEncoder(){
   if(currentState_right != lastState_right && currentState_right == 1){
     tNew_right = millis();
     deltaT_right = tNew_right - tOld_right;
+    deltaCount_right = newCount_right-oldCount_right;
     //thetadot is the angular velocity
-    thetaDot_right = ((newCount_right-oldCount_right)*1000*2*3.1415)/((deltaT_right)*800);
+    thetaDot_right = abs((deltaCount_right*1000*2*3.1415)/((deltaT_right)*800));
+
+    v_RIGHT = thetaDot_right * r;
+    deltaDistance_RIGHT = v_RIGHT * deltaT_right/1000;
+    x_DISTANCE += (cos(phi)*(deltaDistance_LEFT + deltaDistance_RIGHT))/2; // the full x distance the wheel travels
+    y_DISTANCE += (sin(phi)*(deltaDistance_LEFT + deltaDistance_RIGHT))/2; // full y distance the wheel
+    phi += (r/b)*(deltaDistance_LEFT - deltaDistance_RIGHT); // calculates the new angle from perfectly straight
+      
     
     oldCount_right = counter_right;
     if(digitalRead(dt_right) != currentState_right){
